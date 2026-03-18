0161f5b0e9c0aa2f71441dd29851365afaf3e812
Georgi Nikolov <ginikolov24@codingburgas.bg>
2026-03-18 21:25:14 +0200

    Implement main menu with options
    Added a main menu with options to view contents, do a test, or exit the program.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main{
-  //code here
+// Declare the external functions from other files
+int contentsMain();
+int testMain();
+
+int main() {
+    int choice = 0;
+
+    while (true) {
+        cout << "\n=== MAIN MENU ===\n";
+        cout << "1. View contents\n";
+        cout << "2. Do a test\n";
+        cout << "3. Exit\n";
+        cout << "Select an option: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                contentsMain();
+                break;
+
+            case 2:
+                testMain();
+                break;
+
+            case 3:
+                cout << "Exiting program...\n";
+                return 0;
+
+            default:
+                cout << "Invalid option. Try again.\n";
+        }
+    }
 }
