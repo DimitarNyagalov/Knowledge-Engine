f1773d8638099378a2d6a8c738f0fed908a0a8c0
Мартин Желев <MAJelev24@codingburgas.bg>
2026-03-23 20:22:19 +0200

    Update statistics.cpp

--- src/stats/statistics.cpp
+++ src/stats/statistics.cpp
@@ -1,3 +1,16 @@
 #include "statistics.h"
 #include "../stats/statistics.h"
 #include <iostream>
+
+Statistics stats;
+
+void Statistics::update(int score, int totalQuestions) {
+    totalTests++;
+    totalCorrectAnswers += score;
+    totalQuestionsAnswered += totalQuestions;
+
+    if (score > bestScore)
+        bestScore = score;
+
+    if (score < worstScore)
+        worstScore = score;
