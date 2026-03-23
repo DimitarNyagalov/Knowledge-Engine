ba9f374f0b2d6b273100996bc8bfa95957cad2bf
Мартин Желев <MAJelev24@codingburgas.bg>
2026-03-23 20:23:35 +0200

    Update statistics.h

--- src/stats/statistics.h
+++ src/stats/statistics.h
@@ -1,6 +1,16 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
+class Statistics {
+public:
+    int totalTests = 0;
+    int totalCorrectAnswers = 0;
+    int totalQuestionsAnswered = 0;
+    int bestScore = 0;
+    int worstScore = 20;
 
+    void update(int score, int totalQuestions);
+    void display() const;
+};
 
 #endif
