628b5f5f0687192e84536eaa4e17ec61d6d7b62f
Ivan_tr77 <IITrifonov24@codingburgas.bg>
2026-03-23 20:11:33 +0200

    Add Question class with attributes and methods

--- src/models/questions.h
+++ src/models/questions.h
@@ -1,7 +1,20 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 
+#include <string>
+#include <vector>
 
+class Question {
+public:
+    std::string text;
+    std::vector<int> options;   // A, B, C choices
+    char correctOption;         // 'A', 'B', or 'C'
 
+    Question() = default;
+    Question(const std::string& t, const std::vector<int>& opts, char correct);
+
+    void display(int number) const;
+
+};
 
 #endif
