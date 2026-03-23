7f3163ab0e2b2b333f00eebc15092f2cd00cdc3d
DimitarNyagalov <dpnyagalov24@codingburgas.bg>
2026-03-23 20:15:21 +0200

    Update scoring.cpp

--- src/test/scoring.cpp
+++ src/test/scoring.cpp
@@ -1,1 +1,14 @@
 #include "scoring.h"
+
+int scoreTest(const std::vector<Question>& questions,
+              const std::vector<char>& answers) {
+
+    int score = 0;
+
+    for (size_t i = 0; i < questions.size(); i++) {
+        if (answers[i] == questions[i].correctOption)
+            score++;
+    }
+
+    return score;
+}
