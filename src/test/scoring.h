d444226a17bba3eb211b7a0fdc984e2e9f587e22
DimitarNyagalov <dpnyagalov24@codingburgas.bg>
2026-03-23 20:16:22 +0200

    Update scoring.h

--- src/test/scoring.h
+++ src/test/scoring.h
@@ -1,4 +1,10 @@
 #ifndef SCORING_H
 #define SCORING_H
 
+#include <vector>
+#include "../models/question.h"
+
+int scoreTest(const std::vector<Question>& questions,
+              const std::vector<char>& answers);
+
 #endif
