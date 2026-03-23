79ba6456b238021841d997c37df48c888de41d3c
Georgi Nikolov <ginikolov24@codingburgas.bg>
2026-03-23 19:32:26 +0200

    Add initial contents.cpp file with main function

---
+++ src/ui/contents.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include "math_contents.h"
+#include "../stats/statistics.h"
+#include "../test/generator.h"
+#include "../test/scoring.h"
+
+int main(){
+
+}
