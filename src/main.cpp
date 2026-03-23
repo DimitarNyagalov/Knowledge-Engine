56cc7e84a7c57cf8ed3f8b516c7537a24d37e845
DimitarNyagalov <dpnyagalov24@codingburgas.bg>
2026-03-23 20:27:50 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,7 @@
 #include <iostream>
 #include "test.h"
 using namespace std;
-
-// Declare the external functions from other files
-int contentsMain();
-
-int main() {
-    int choice = 0;
-
-    while (true) {
-        cout << "\n=== MAIN MENU ===\n";
-        cout << "1. View contents\n";
-        cout << "2. Do a test\n";
-        cout << "3. Exit\n";
-        cout << "Select an option: ";
-        cin >> choice;
-
-        switch (choice) {
-            case 1:
-                contentsMain();
-                break;
-
-            case 2:
-                testMain();
-                break;
-
-            case 3:
-                cout << "Exiting program...\n";
-                return 0;
-
-            default:
-                cout << "Invalid option. Try again.\n";
-        }
-    }
+int main()
+{
+    
 }
